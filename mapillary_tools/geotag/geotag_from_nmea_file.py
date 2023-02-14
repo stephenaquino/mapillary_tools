ca0a5476036526f58c6027c7381f414790ac1ac0
stephenaquino <125326515+stephenaquino@users.noreply.github.com>
2023-02-13 18:06:47 -0800

    Update geotag_from_nmea_file.py

--- mapillary_tools/geotag/geotag_from_nmea_file.py
+++ mapillary_tools/geotag/geotag_from_nmea_file.py
@@ -33,7 +33,7 @@ def get_lat_lon_time_from_nmea(nmea_file: Path) -> T.List[geo.Point]:
 
     # Get initial date
     for l in lines:
-        if "GPRMC" in l:
+        if "GNRMC" in l:
             data = pynmea2.parse(l)
             date = data.datetime.date()
             break
@@ -41,11 +41,11 @@ def get_lat_lon_time_from_nmea(nmea_file: Path) -> T.List[geo.Point]:
     # Parse GPS trace
     points = []
     for l in lines:
-        if "GPRMC" in l:
+        if "GNRMC" in l:
             data = pynmea2.parse(l)
             date = data.datetime.date()
 
-        if "$GPGGA" in l:
+        if "$GNGGA" in l:
             data = pynmea2.parse(l)
             dt = datetime.datetime.combine(date, data.timestamp)
             lat, lon, alt = data.latitude, data.longitude, data.altitude
